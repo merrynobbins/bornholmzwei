bf21f0c131d945cfc0219f32878d7bc6f8f7a0fb
Tobias Winkler <info@tobiaswinkler.berlin>
2016-02-04 23:34:38 +0100

    added directive teaser-anchor

---
+++ app/directives/teaser-anchor/teaser-anchor-directive.js
@@ -0,0 +1,43 @@
+(function () {
+  'use strict';
+
+  /**
+   * @ngdoc directive
+   * @name bornholmZwei.directive:teaserAnchor
+   * @restrict EA
+   * @element
+   *
+   * @description
+   *
+   * @example
+     <example module="bornholmZwei">
+       <file name="index.html">
+        <teaser-anchor></teaser-anchor>
+       </file>
+     </example>
+   *
+   */
+  angular
+    .module('bornholmZwei')
+    .directive('teaserAnchor', teaserAnchor);
+
+  function teaserAnchor() {
+    return {
+      restrict: 'EA',
+      scope: false,
+      templateUrl: 'directives/teaser-anchor/teaser-anchor-directive.tpl.html',
+      replace: true,
+      controllerAs: 'teaserAnchor',
+      controller: function () {
+        var vm = this;
+        vm.name = 'teaserAnchor';
+      },
+      link: function (scope, element, attrs) {
+        /* jshint unused:false */
+        /* eslint "no-unused-vars": [2, {"args": "none"}] */
+
+        scope.content = attrs.content;
+      }
+    };
+  }
+}());

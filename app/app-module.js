9329407c0d2060f9fa5befdc89e073c9ce56d7b4
Tobias Winkler <info@tobiaswinkler.berlin>
2016-02-04 22:20:40 +0100

    initial commit

---
+++ app/app-module.js
@@ -0,0 +1,15 @@
+(function () {
+  'use strict';
+
+  /* @ngdoc object
+   * @name bornholmZwei
+   * @description
+   *
+   */
+  angular
+    .module('bornholmZwei', [
+      'ui.router',
+      'mm.foundation',
+      'home'
+    ]);
+}());

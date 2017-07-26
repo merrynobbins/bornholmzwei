806964b8dd6e10ae2f4187a7d2ebcce2d7951848
S. Maerten <merrynobbins@gmail.com>
2017-07-26 19:53:42 +0200

    added files for module 'Datenschutzerklärung'

---
+++ app/datenschutz/datenschutz-module.js
@@ -0,0 +1,13 @@
+(function () {
+  'use strict';
+
+  /* @ngdoc object
+   * @name datenschutz
+   * @description
+   *
+   */
+  angular
+    .module('datenschutz', [
+      'ui.router'
+    ]);
+}());
